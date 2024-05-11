e2c7b60325c63dad872db7f0465741703ffdf25d
HakiPintolli <haki.pintolli@student.uni-pr.edu>
2024-05-11 20:36:18 +0200

    update

--- src/main/java/com/example/bankprojekt/Controller/LoginController.java
+++ src/main/java/com/example/bankprojekt/Controller/LoginController.java
@@ -1,4 +1,12 @@
 package com.example.bankprojekt.Controller;
 
+import javafx.scene.control.*;
+
 public class LoginController {
+    public ChoiceBox ac_selector;
+    public Label payee_adress_lbl;
+    public TextField payee_adress_fld;
+    public PasswordField password_fld;
+    public Button login_btn;
+    public Label error_lbl;
 }
