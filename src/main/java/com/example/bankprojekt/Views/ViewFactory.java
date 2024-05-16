2266d697b67318aba1bf8a7be29d68b262e040be
FikriS-404 <fikri.stublla@student.uni-pr.edu>
2024-05-16 14:39:08 +0200

    Trainsactions 1.2

--- src/main/java/com/example/bankprojekt/Views/ViewFactory.java
+++ src/main/java/com/example/bankprojekt/Views/ViewFactory.java
@@ -1,4 +1,21 @@
 package com.example.bankprojekt.Views;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
 public class ViewFactory {
+
+    private AnchorPane transactionsView;
+
+
+    public AnchorPane getTransactionsView() {
+        if (transactionsView==null){
+            try {
+                transactionsView = new FXMLLoader(getClass().getResource("/Fxml/Client/Transactions.fmxl")).load();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return transactionsView;
+    }
 }
