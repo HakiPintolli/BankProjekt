8184b13eead9557949c4031c8c278fc3988b9581
emirbislimi <153293785+emirbislimi@users.noreply.github.com>
2024-05-19 22:16:34 +0200

    Update ClientMenuController.java

--- src/main/java/com/example/bankprojekt/Controller/Client/ClientMenuController.java
+++ src/main/java/com/example/bankprojekt/Controller/Client/ClientMenuController.java
@@ -1,5 +1,19 @@
 package com.example.bankprojekt.Controller.Client;
 
-public class ClientMenuController {
+import javafx.fxml.Initializable;
+import jacafx.scene.control.Button;
 
+import java.net.URL;
+import java.util.resourceBundle;
+
+public class ClientMenuController implements Initializable {
+  public Button dashboard_btn;
+  public Button transaction_btn;
+  public Button accounts_btn;
+  public Button profile_btn;
+  public Button logout_btn;
+  public Button report_btn;
+
+  @Override
+  public void initilaze(URL url, ResourceBundle resourceBundle){}
 }
