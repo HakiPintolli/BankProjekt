687ef5d76ebcea8dc838c6d6ee5de0bcb90e01bc
emirbislimi <153293785+emirbislimi@users.noreply.github.com>
2024-05-24 19:45:07 +0200

    Update DashboardController.java

--- src/main/java/com/example/bankprojekt/Controller/Client/DashboardController.java
+++ src/main/java/com/example/bankprojekt/Controller/Client/DashboardController.java
@@ -1,4 +1,27 @@
 package com.example.bankprojekt.Controller.Client;
 
-public class DashboardController {
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.net.ResourceBundle;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    public Text user_name;
+    public label login_date;
+    public label checking_bal;
+    public label checking_acc_num;
+    public label savings_bal;
+    public label savings_acc_num;
+    public label income_lbl;
+    public label expense_lbl;
+    public ListView transaction_listview;
+    public TextField payee_fld;
+    public TextField amount_fld;
+
+    @Override
+    public void initiaize(URL url, ResourceBundle resourceBundle){}
 }
