59e952e8d0fb1ca457bb0ae672a9bac01e253a72
HakiPintolli <155005609+HakiPintolli@users.noreply.github.com>
2024-05-25 23:16:00 +0200

    Add files via upload

---
+++ src/main/java/com/example/bankprojekt/Controller/Client/TransactionCellController.java
@@ -0,0 +1,22 @@
+package com.example.bankprojekt.Controller.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransactionCellController implements Initializable {
+
+    public Label transaction_date;
+    public ImageView icon;
+    public Label sender_lbl;
+    public Label seciver_lbl;
+    public Label amount_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
