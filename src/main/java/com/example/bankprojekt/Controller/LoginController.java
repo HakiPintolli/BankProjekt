e7fa34cbedd12f8a8b7fcc984d026452deb3eff9
emirbislimi <153293785+emirbislimi@users.noreply.github.com>
2024-05-25 06:17:56 +0200

    Update LoginController.java

--- src/main/java/com/example/bankprojekt/Controller/LoginController.java
+++ src/main/java/com/example/bankprojekt/Controller/LoginController.java
@@ -1,12 +1,28 @@
 package com.example.bankprojekt.Controller;
 
+import com.example.bankprojekt.Models.Model;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class LoginController implements Initializable{
     public ChoiceBox ac_selector;
     public Label payee_adress_lbl;
     public TextField payee_adress_fld;
     public PasswordField password_fld;
     public Button login_btn;
     public Label error_lbl;
+    @Override
+    public void initialize(URL url,ResourceBundle resourceBundle){
+        login_btn.setOnAction(event -> Model.getInstance().getViewFactory().showClientWindow());
+    }
+    private void onLogin(){
+        Stage stage = (Stage)error_lbl.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showClientWindow();
+    }
 }
