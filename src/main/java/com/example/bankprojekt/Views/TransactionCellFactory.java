55797b025d44e0e210a26638ee6d511b10d32cd8
fatimess <148488430+fatimess@users.noreply.github.com>
2024-05-25 23:45:27 +0200

    Create TransactionCellFactory.java

---
+++ src/main/java/com/example/bankprojekt/Views/TransactionCellFactory.java
@@ -0,0 +1,29 @@
+package com.example.bankprojekt.Views;
+
+import com.example.bankprojekt.Controller.Client.TransactionCellController;
+
+import com.example.bankprojekt.Models.Transaction;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListCell;
+
+public class TransactionCellFactory extends ListCell<Transaction> {
+    @Override
+    protected void updateItem(Transaction transaction, boolean empty) {
+        super.updateItem(transaction, empty);
+        if (empty){
+            setText(null);
+            setGraphic(null);
+
+        }else{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/TransactionCell.fxml"));
+            TransactionCellController controller = new TransactionCellController(transaction);
+            loader.setController(controller);
+            setText(null);
+            try {
+                setGraphic(loader.load());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
