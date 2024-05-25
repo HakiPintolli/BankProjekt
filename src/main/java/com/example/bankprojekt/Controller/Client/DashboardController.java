3e9c420a88f256e08e29cccd42249d01c66ae8b0
emirbislimi <153293785+emirbislimi@users.noreply.github.com>
2024-05-25 09:23:10 +0200

    Update DashboardController.java

--- src/main/java/com/example/bankprojekt/Controller/Client/DashboardController.java
+++ src/main/java/com/example/bankprojekt/Controller/Client/DashboardController.java
@@ -6,18 +6,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.net.URL;
-import java.net.ResourceBundle;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
     public Text user_name;
-    public label login_date;
-    public label checking_bal;
-    public label checking_acc_num;
-    public label savings_bal;
-    public label savings_acc_num;
-    public label income_lbl;
-    public label expense_lbl;
+    public Label login_date;
+    public Label checking_bal;
+    public Label checking_acc_num;
+    public Label savings_bal;
+    public Label savings_acc_num;
+    public Label income_lbl;
+    public Label expense_lbl;
     public ListView transaction_listview;
     public TextField payee_fld;
     public TextField amount_fld;
