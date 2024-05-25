72f1c58b05e78dc0b37650b6bf6a23bd01fd3b67
fatimess <148488430+fatimess@users.noreply.github.com>
2024-05-25 20:28:15 +0200

    Update App.java

--- src/main/java/com/example/bankprojekt/App.java
+++ src/main/java/com/example/bankprojekt/App.java
@@ -8,11 +8,9 @@ import javafx.stage.Stage;
 public class App extends Application{
 
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
+ public void start(Stage stage){
+       Model.getInstance().getViewFactory().showLoginWindow();
+
     }
     
 }
